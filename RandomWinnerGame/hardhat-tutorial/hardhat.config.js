440e092858a01af4470387ef209562accef0b063
Tsuhito <enescxndev@gmail.com>
2023-12-25 21:34:24 +0100

    Initialize subgraph

---
+++ RandomWinnerGame/hardhat-tutorial/hardhat.config.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox");
+
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const POLYGONSCAN_KEY = process.env.POLYGONSCAN_KEY;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    mumbai: {
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: POLYGONSCAN_KEY,
+    },
+  },
+};
---
+++ RandomWinnerGame/hardhat-tutorial/scripts/deploy.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+
+const { ethers } = require("hardhat");
+const { FEE, VRF_COORDINATOR, LINK_TOKEN, KEY_HASH } = require("../constants");
+
+async function main() {
+  /**
+   * DeployContract in ethers.js is an abstraction used to deploy new smart contracts, so randomWinnerGame here is 
+   * a factory for instances of our RandomWinnerGame contract.
+   */
+
+  // Deploy the contract
+  const randomWinnerGame = await hre.ethers.deployContract("RandomWinnerGame", [VRF_COORDINATOR, LINK_TOKEN, KEY_HASH, FEE]);
+
+  await randomWinnerGame.waitForDeployment();
+
+  // print the address of the deployed contract
+  console.log("Verify Contract Address:", randomWinnerGame.target);
+
+  console.log("Sleeeping.....");
+
+  // Wait for etherscan to notice that the contract has been deployed
+  await sleep(30000);
+
+  // Verify the contract after deploying 
+  await hre.run("verify:verify", {
+    address: randomWinnerGame.target,
+    constructorArguments: [VRF_COORDINATOR, LINK_TOKEN, KEY_HASH, FEE],
+  });
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+// Call the main function and catch if there is any error
+main().then(() => process.exit(0)).catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
